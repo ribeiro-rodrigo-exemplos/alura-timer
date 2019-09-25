b9b14d27c5b580b9b080ae99e867ef441cdaea8c
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2019-09-25 07:48:54 -0300

    aula sobre ipc

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,1 +1,7 @@
-console.log('Eu estou na Janela do Browser'); 
+const { ipcRenderer } = require('electron')
+
+let linkSobre = document.querySelector("#link-sobre")
+
+linkSobre.addEventListener('click', function () {
+    ipcRenderer.send('abrir-janela-sobre')
+})
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 app.on('ready', () => {
     console.log('Aplicação iniciada');
@@ -12,4 +12,13 @@ app.on('ready', () => {
 
 app.on('window-all-closed', () => {
     app.quit();
+})
+
+ipcMain.on('abrir-janela-sobre', () => {
+    let sobreWindow = new BrowserWindow({
+        width: 300,
+        height: 200
+    })
+
+    sobreWindow.loadURL(`file://${__dirname}/app/sobre.html`)
 })
