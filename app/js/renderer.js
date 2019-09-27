f44b5bde7f7ac77e0b4de26547e41c6377f3ebd5
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2019-09-26 23:52:20 -0300

    finalizando unidade 4

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,28 +1,37 @@
-const { ipcRenderer } = require('electron'); 
-const timer = require('./timer'); 
+const { ipcRenderer } = require('electron');
+const timer = require('./timer');
+const data = require('../../data');
 
 let linkSobre = document.querySelector("#link-sobre")
 let botaoPlay = document.querySelector('.botao-play')
-let tempo = document.querySelector('.tempo'); 
-let curso = document.querySelector('.curso'); 
+let tempo = document.querySelector('.tempo');
+let curso = document.querySelector('.curso');
+
+window.onload = () => {
+    data.pegaDados(curso.textContent)
+        .then((dados) => {
+            console.log(dados)
+            tempo.textContent = dados.tempo
+        })
+}
 
 linkSobre.addEventListener('click', function () {
     ipcRenderer.send('abrir-janela-sobre')
 })
 
 let imgs = ['img/play-button.svg', 'img/stop-button.svg']
-let play = false; 
+let play = false;
 
-botaoPlay.addEventListener('click',function(){
+botaoPlay.addEventListener('click', function () {
 
-    if(play){
+    if (play) {
         timer.parar(curso.textContent)
-        play = false 
-    }else{
+        play = false
+    } else {
         timer.iniciar(tempo);
-        play = true  
+        play = true
     }
 
-    imgs = imgs.reverse(); 
-    botaoPlay.src = imgs[0]; 
+    imgs = imgs.reverse();
+    botaoPlay.src = imgs[0];
 })
--- data.js
+++ data.js
@@ -1,24 +1,40 @@
 const jsonfile = require('jsonfile-promised')
-const fs = require('fs'); 
+const fs = require('fs');
 
 module.exports = {
-    salvaDados(curso,tempoEstudado){
-        let arquivoDoCurso = __dirname + '/data/' + curso + '.json'; 
-        if(fs.existsSync(arquivoDoCurso)){
-
-        }else{
-            this.criaArquivoDeCurso(arquivoDoCurso,{})
+    salvaDados(curso, tempoEstudado) {
+        let arquivoDoCurso = __dirname + '/data/' + curso + '.json';
+        if (fs.existsSync(arquivoDoCurso)) {
+            this.adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado)
+        } else {
+            this.criaArquivoDeCurso(arquivoDoCurso, {})
                 .then(() => {
-
-                }) 
+                    this.adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado)
+                })
         }
-    }, 
-    criaArquivoDeCurso(nomeArquivo, conteudoArquivo){
-        return jsonfile.writeFile(nomeArquivo,conteudoArquivo)
-                .then(() => {
-                    console.log('Arquivo Criado'); 
-                }).catch((err) => {
-                    console.log(err); 
-                }); 
+    },
+    adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado) {
+        let dados = {
+            ultimoEstudo: new Date().toString(),
+            tempo: tempoEstudado
+        }
+        jsonfile.writeFile(arquivoDoCurso, dados, { spaces: 2 })
+            .then(() => {
+                console.log('Tempo salvo com sucesso')
+            }).catch(err => {
+                console.log(err);
+            })
+    },
+    criaArquivoDeCurso(nomeArquivo, conteudoArquivo) {
+        return jsonfile.writeFile(nomeArquivo, conteudoArquivo)
+            .then(() => {
+                console.log('Arquivo Criado');
+            }).catch((err) => {
+                console.log(err);
+            });
+    },
+    pegaDados(nomeCurso) {
+        let arquivoDoCurso = __dirname + '/data/' + nomeCurso + '.json';
+        return jsonfile.readFile(arquivoDoCurso);
     }
 }
